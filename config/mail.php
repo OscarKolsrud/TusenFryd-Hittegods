d2dd174b40ad2e2783f5647c01fb4bc8cae21e81
Oscar S Kolsrud <oscar.sortland.kolsrud@gmail.com>
2020-06-05 09:00:00 +0200

    Fixed mail errors occuring due to the mail server setup

--- config/mail.php
+++ config/mail.php
@@ -33,12 +33,21 @@ return [
     |
     */
 
+    //This is used for work around with Tusenfryds email setup and should not be used when not needed
+    'stream' => [
+        'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true,
+        ],
+    ],
+
     'mailers' => [
         'smtp' => [
             'transport'  => 'smtp',
             'host'       => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port'       => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'encryption' => env('MAIL_ENCRYPTION', ''),
             'username'   => env('MAIL_USERNAME'),
             'password'   => env('MAIL_PASSWORD'),
             'timeout'    => null,
