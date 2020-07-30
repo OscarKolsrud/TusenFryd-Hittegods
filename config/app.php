f3d97079ae110f4a381a4bf07f38f704e29f93f1
Oscar S Kolsrud <30559109+OscarKolsrud@users.noreply.github.com>
2020-07-30 16:26:28 +0200

    Correct timezone

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Europe/Oslo',
 
     /*
     |--------------------------------------------------------------------------
@@ -77,7 +77,7 @@ return [
     | The application locale determines the default locale that will be used
     | by the translation service provider. You are free to set this value
     | to any of the locales which will be supported by the application.
-    |
+    |ss
     */
 
     'locale' => env('APP_LOCALE', 'en'),
